043fa838954815f05dcbbbe2fdb54bb1b1a894a1
Anuj Karambalkar <anujkarambalkar1504@gmail.com>
2025-06-10 22:46:47 +0530

    Initialize Structure with Front&Back End and done with signup,login,profile pages

---
+++ backend/src/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT;
+
+// ✅ Increase payload size limits to support base64 images
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server is running on PORT:" + PORT);
+  connectDB();
+});
